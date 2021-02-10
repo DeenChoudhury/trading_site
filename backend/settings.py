6affd547c2130845b7e5b14ccec2c74711aa042f
Deen Choudhury <deen.r.choudhury@gmail.com>
2021-02-10 15:07:46 -0500

    blah blah 4.0

--- backend/settings.py
+++ backend/settings.py
@@ -137,7 +137,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
-REACT_APP_DIR = os.path.join(BASE_DIR, 'frontend')
+REACT_APP_DIR = os.path.join(BASE_DIR, '')
 
 STATICFILES_DIRS = [
     os.path.join(REACT_APP_DIR, 'build', 'static'),
